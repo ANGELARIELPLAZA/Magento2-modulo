1d99de1678fca4cb83dd9351207600824f13b5c5
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-10 13:52:14 -0600

    Update magento.sh

--- magento.sh
+++ magento.sh
@@ -12,21 +12,20 @@
 echo "Hola bienvenido a la instalacion de magento2.4"
 read -p "Deseas continuar:[y/n]" option
 if  [ $option = "y" ] ; then
-    
-    echo "\n***************APACHE**********************\n"
+    echo "***************APACHE**********************"
     sudo apt -y install apache2
-    echo "\n version instalada: \n"
+    echo " version instalada: "
     sudo apache2ctl -v
-    echo "\n Vamos a verificar el cortafuegos con los siguientes comandos, uno por uno: \n"
+    echo " Vamos a verificar el cortafuegos con los siguientes comandos, uno por uno: "
     sudo ufw app list
     sudo ufw allow 'Apache'
     sudo ufw status
-    echo "\n Vamos a verificar el servidor apache con los siguientes comandos: \n"
+    echo " Vamos a verificar el servidor apache con los siguientes comandos: "
     sudo systemctl start apache2
     sudo systemctl status --no-pager apache2
     sudo systemctl enable apache2
-    echo "\n Vamos a configurar nuestro Host Virtual, esto para asignarle un dominio, con los siguientes
-    comandos, tomaremos como nombre “magento2”: \n"
+    echo " Vamos a configurar nuestro Host Virtual, esto para asignarle un dominio, con los siguientes
+    comandos, tomaremos como nombre “magento2”: "
     if [ -d /var/www/html/magento2 ];
     then
         echo "Sí, sí existe /var/www/html/magento2."
@@ -41,8 +40,7 @@ if  [ $option = "y" ] ; then
         echo "No, no existe /var/www/html/magento2 - se acaba de crear"
         sudo mkdir +x /var/www/html/magento2
     fi
-    
-    sudo rm /etc/apache2/sites-available/000-default.conf
+    sudo rm /etc/apache2/sites-available/000-default.conf 
     sudo touch /etc/apache2/sites-available/000-default.conf
     sudo chmod +x /etc/apache2/sites-available/000-default.conf
     sudo echo -e "
@@ -57,11 +55,9 @@ if  [ $option = "y" ] ; then
     " >>/etc/apache2/sites-available/000-default.conf
     cat /etc/apache2/sites-available/000-default.conf
     sudo echo "ServerName 127.0.1.1" >>  /etc/apache2/apache2.conf
-    
     sudo a2ensite 000-default.conf
     sudo apache2ctl configtest
     sudo systemctl restart apache2
-    
     echo "***************MYSQL**********************"
     sudo apt  -y install mysql-server
     mysql -u root -p'admin1234' -e "CREATE USER magento2@localhost IDENTIFIED BY 'admin1234'; ALTER USER 'magento2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin1234'; GRANT ALL PRIVILEGES ON *.* TO 'magento2'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;"
@@ -80,10 +76,8 @@ if  [ $option = "y" ] ; then
     <IfModule mod_dir.c>
     DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
     </IfModule>
-
     # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
     " >>/etc/apache2/mods-enabled/dir.conf
-    
     sudo a2enmod rewrite
     sudo apt -y install  php7.4-bcmath php7.4-intl php7.4-soap php7.4-zip php7.4-gd php7.4-json php7.4-curl php7.4-cli php7.4-xml php7.4-xmlrpc php7.4-gmp php7.4-common
     sudo systemctl reload apache2
@@ -98,7 +92,6 @@ if  [ $option = "y" ] ; then
     sudo apt update
     sudo apt -y install  elasticsearch=7.9.3
     sudo a2enmod proxy && sudo a2enmod proxy_http && sudo service apache2 restart
-    
     sudo rm /etc/apache2/ports.conf
     sudo touch /etc/apache2/ports.conf
     sudo chmod +x /etc/apache2/ports.conf
@@ -120,19 +113,16 @@ if  [ $option = "y" ] ; then
     ProxyPass "/" "http://localhost:9200/"
     ProxyPassReverse "/" "http://localhost:9200/"
     </VirtualHost> ' >>   /etc/apache2/sites-available/000-default.conf
-    
     sudo a2ensite 000-default.conf
     sudo service apache2 restart
     sudo systemctl start elasticsearch
     sudo systemctl enable elasticsearch
     curl -i http://localhost:8080/_cluster/health
     curl -X GET "localhost:9200"
-    
     echo "***************Composer**********************"
     sudo curl -sS https://getcomposer.org/installer -o composer-setup.php
     sudo php composer-setup.php --install-dir=/usr/bin --filename=composer -y
     composer
-    
     echo "***************Magento**********************"
     cd /var/www/html
     sudo rm -r magento2
@@ -143,9 +133,8 @@ if  [ $option = "y" ] ; then
     sudo chown -R :www-data .
     sudo chmod u+x /var/www/html/magento2/bin/magento
     cd /var/www/html/magento2
-    
     sudo php bin/magento setup:install \
-    --base-url=http://192.168.0.36 \
+    --base-url=http://192.168.0.37 \
     --db-host=localhost \
     --db-name=magento2 \
     --db-user=magento2 \
@@ -162,7 +151,6 @@ if  [ $option = "y" ] ; then
     --search-engine=elasticsearch7 \
     --elasticsearch-host=localhost \
     --elasticsearch-port=8080
-    
     sudo rm /etc/apache2/sites-available/000-default.conf
     sudo touch /etc/apache2/sites-available/000-default.conf
     sudo chmod +x /etc/apache2/sites-available/000-default.conf
@@ -180,23 +168,19 @@ if  [ $option = "y" ] ; then
     </VirtualHost>
     " >>/etc/apache2/sites-available/000-default.conf
     cat /etc/apache2/sites-available/000-default.conf
-    
     sudo a2ensite 000-default.conf
     sudo apache2ctl configtest
     sudo systemctl restart apache2
-    
-    sudo mysql -u magento2 -p'admin1234' -e "use magento2; UPDATE core_config_data SET value='http://192.168.0.36' WHERE path='web/unsecure/base_url';"
+    sudo mysql -u magento2 -p'admin1234' -e "use magento2; UPDATE core_config_data SET value='http://192.168.0.37' WHERE path='web/unsecure/base_url';"
     sudo php /var/www/html/magento2/bin/magento deploy:mode:set production
     sudo php /var/www/html/magento2/bin/magento cache:flush
     sudo chmod -R 777 /var/www/html/magento2/var
     sudo chmod -R 777 /var/www/html/magento2/pub/static
     sudo chmod -R 777 /var/www/html/magento2/generated
     sudo chmod -R 777 /var/www/html/magento2/generated/
-    
     sudo  php /var/www/html/magento2/bin/magento s:up
     sudo  php /var/www/html/magento2/bin/magento s:s:d -f
     sudo  php /var/www/html/magento2/bin/magento c:f
-       
     sudo php /var/www/html/magento2/bin/magento module:disable Magento_TwoFactorAuth
     
 else
