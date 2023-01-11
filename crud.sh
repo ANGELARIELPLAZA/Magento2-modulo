47e9900b7c608542cd5e2e0c8978c0df11e3c985
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-11 08:37:12 -0600

    update

--- crud.sh
+++ crud.sh
@@ -7,23 +7,23 @@ sudo mkdir -p /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Model/Resourc
 sudo mkdir -p /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Controller/Index/
 sudo mkdir -p /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/
 #Step 1: Setup Script
-sudo mv crud/InstallSchema.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/InstallSchema.php
-sudo rm /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/module.xml
-sudo mv crud/module.xml  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/module.xml
-sudo mv crud/UpgradeSchema.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/UpgradeSchema.php
-sudo mv crud/UpgradeData.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/UpgradeData.php
+sudo mv crud/InstallSchema.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/
+sudo rm /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/
+sudo mv crud/module.xml  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/
+sudo mv crud/UpgradeSchema.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/
+sudo mv crud/UpgradeData.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/
 sudo php /var/www/html/magento2/bin/magento setup:upgrade
 sudo php /var/www/html/magento2/bin/magento setup:static-content:deploy es_MX en_US -f
-sudo mv crud/InstallData.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/InstallData.php
+sudo mv crud/InstallData.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Setup/
 #Step 2: Create Model
-sudo mv crud/Post.php  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Model/Post.php
+sudo mv crud/Post.php  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Model/
 #Step 3: Create Resource Model
-sudo mv crud/ResourceModel/Post.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Model/ResourceModel/Post.php
+sudo mv crud/ResourceModel/Post.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Model/ResourceModel/
 #Step 4: Create Resource Model Collection
-sudo mv crud/Collection.php  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Model/ResourceModel/Post/Collection.php
+sudo mv crud/Collection.php  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Model/ResourceModel/Post/
 #Step 5: Factory Object
-sudo rm /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
-sudo mv crud/Index.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
+sudo rm /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Controller/Index/
+sudo mv crud/Index.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Controller/Index/
 sudo php /var/www/html/magento2/bin/magento setup:upgrade
 sudo php /var/www/html/magento2/bin/magento setup:di:compile
 sudo php /var/www/html/magento2/bin/magento setup:static-content:deploy es_MX en_US -f
@@ -32,5 +32,3 @@ sudo php /var/www/html/magento2/bin/magento cache:clean
 sudo php /var/www/html/magento2/bin/magento cache:flush
 sudo chown -R www-data:www-data /var/www/html/magento2/
 sudo chmod 777 -R /var/www/html/magento2/
-sudo php /var/www/html/magento2/bin/magento setup:InstallData run
-
--- mageplaza.sh
+++ mageplaza.sh
@@ -1,5 +1,4 @@
 #Step 1: Create the folder of Hello World module
-cd /var/www/html/magento2/
 sudo mkdir -p /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/frontend/
 sudo mkdir -p /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Controller/Index/
 sudo mkdir -p /var/www/html/magento2/app/code/Mageplaza/HelloWorld/view/frontend/layout/
@@ -7,9 +6,9 @@ sudo mkdir -p /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Block/
 sudo mkdir -p  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/view/frontend/templates/
 
 #Step 2: Create etc/module.xml file.
-sudo mv modulo/module.xml  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/module.xml
+sudo mv modulo/module.xml  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/
 #Step 3: Create etc/registration.php file
-sudo mv modulo/registration.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/registration.php
+sudo mv modulo/registration.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/
 #Step 4: Enable the module
 sudo php /var/www/html/magento2/bin/magento module:status
 sudo php /var/www/html/magento2/bin/magento module:enable Mageplaza_HelloWorld
@@ -20,15 +19,15 @@ sudo php /var/www/html/magento2/bin/magento setup:static-content:deploy -f
 
 #Create Controller in Magento 2
 #Step 1: Create routes.xml file
-sudo mv modulo/routes.xml  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/frontend/routes.xml
+sudo mv modulo/routes.xml  /var/www/html/magento2/app/code/Mageplaza/HelloWorld/etc/frontend/
 #Step 2: Create controller file
-sudo mv modulo/Index.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
+sudo mv modulo/Index.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Controller/Index/
 #Step 3: Create controller Layout file
-sudo mv modulo/helloworld_index_index.xml /var/www/html/magento2/app/code/Mageplaza/HelloWorld/view/frontend/layout/helloworld_index_index.xml
+sudo mv modulo/helloworld_index_index.xml /var/www/html/magento2/app/code/Mageplaza/HelloWorld/view/frontend/layout/
 #Step 4: Create controller Block file
-sudo mv modulo/Block/Index.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Block/Index.php
+sudo mv modulo/Block/Index.php /var/www/html/magento2/app/code/Mageplaza/HelloWorld/Block/
 #Step 5: Create controller template file
-sudo mv modulo/index.phtml /var/www/html/magento2/app/code/Mageplaza/HelloWorld/view/frontend/templates/index.phtml
+sudo mv modulo/index.phtml /var/www/html/magento2/app/code/Mageplaza/HelloWorld/view/frontend/templates/
 #Step 6: Flush Magento cache
 sudo php /var/www/html/magento2/bin/magento cache:clean
 sudo php /var/www/html/magento2/bin/magento cache:flush
