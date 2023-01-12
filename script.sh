40ff8111d2e83ceaf1ffdfe20a0402e9da3c1b89
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-12 10:47:14 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
 read -p "Nombre del vendor: " var1
 read -p "Nombre del modulo: " var2
-sudo sed -i s/Mageplaza/$var1/g "modulo/1.modulo.sh"
-cat modulo/1.modulo.sh
-sudo sed -i s/$var1/Mageplaza/g "modulo/1.modulo.sh"
-cat modulo/1.modulo.sh
 
+sudo sed -i s/Mageplaza/$var1/g "modulo/1.modulo.sh"
 sudo sed -i s/HelloWorld/$var2/g "modulo/1.modulo.sh"
 cat modulo/1.modulo.sh
+
 sudo sed -i s/$var2/HelloWorld/g "modulo/1.modulo.sh"
+sudo sed -i s/$var1/Mageplaza/g "modulo/1.modulo.sh"
 cat modulo/1.modulo.sh
